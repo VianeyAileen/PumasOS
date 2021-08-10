e9ca3f08f1dc7281689538e35903abed31442107
99joseluis <jose_luis@ciencias.unam.mx>
2021-08-09 22:07:47 -0500

    Modificando la base de datos y quitando algunos detalles de producto rest

--- BreakBlue Market/API/SQL/BD.sql
+++ BreakBlue Market/API/SQL/BD.sql
@@ -9,9 +9,6 @@ CREATE TABLE `producto` (
     `marca` varchar(50) NOT NULL,
     `descripcion` varchar(500) NOT NULL,
     `unidadesDisponibles` int NOT NULL,
-    `imagen` varchar(2000) NOT NULL,
-    `calificacion` float(10) NOT NULL,
-    `comentario` varchar(500) NOT NULL,
     `correo` varchar(500) NOT NULL,
 
     PRIMARY KEY (`id`),
@@ -25,9 +22,24 @@ CREATE TABLE `imagen` (
 	`id` int(11) NOT NULL auto_increment,
     `imagen` varchar(2000) NOT NULL,
 
-    PRIMARY KEY (`id`),
-    UNIQUE(`id`)
-    ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+    FOREIGN KEY imagen(`id`) REFERENCES producto(`id`)
+    ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `comentario`;
+CREATE TABLE `comentario` (
+	`id` int(11) NOT NULL auto_increment,
+    `comentario` varchar(2000) NOT NULL,
+
+	FOREIGN KEY comentario(`id`) REFERENCES producto(`id`)
+    ) ENGINE=MyISAM DEFAULT CHARSET=latin1 ;
+    
+DROP TABLE IF EXISTS `calificacion`;
+CREATE TABLE `calificacion` (
+	`id` int(11) NOT NULL auto_increment,
+    `calificacion` float(10) NOT NULL,
+    
+	FOREIGN KEY calificacion(`id`) REFERENCES producto(`id`)
+    ) ENGINE=MyISAM DEFAULT CHARSET=latin1 ;
 
 DROP TABLE IF EXISTS `comprador`;
 CREATE TABLE `comprador` (
@@ -35,7 +47,7 @@ CREATE TABLE `comprador` (
 	`nombre` varchar(50) NOT NULL,
     `apellidos` varchar(50) NOT NULL,
     `contrasena` varchar(255) NOT NULL,
-    `nombreUsuario` varchar(10) NOT NULL,
+    `nombreUsuario` varchar(20) NOT NULL,
 
     PRIMARY KEY (`correo`),
     UNIQUE(`correo`)
@@ -47,7 +59,7 @@ CREATE TABLE `vendedor` (
 	`nombre` varchar(50) NOT NULL,
     `apellidos` varchar(50) NOT NULL,
     `contrasena` varchar(255) NOT NULL,
-    `nombreUsuario` varchar(10) NOT NULL,
+    `nombreUsuario` varchar(20) NOT NULL,
 
     PRIMARY KEY (`correo`),
     UNIQUE(`correo`)
@@ -58,11 +70,70 @@ CREATE TABLE `vendedor` (
     SELECT * FROM comprador;
     SELECT * FROM vendedor;
     SELECT * FROM imagen;
-
-	INSERT INTO `producto` VALUES (1, 'Celular', 20.00, 'Apple', 'Bonito celular 100% calidad', 11, '/home/vianey/Descargas/sample1.jpg', 4.2, 'Estuvo bien chafa, muy pirata', 'micorreito@feliz.com');
-	INSERT INTO `imagen` VALUES (1, '/home/vianey/Descargas/sample1.jpg');
-	INSERT INTO `comprador` VALUES ('micorreito@correo.com', 'Rory', 'García', 'pas123', 'rorgar');
-	INSERT INTO `vendedor` VALUES ('vendedor@correo.com', 'Maria', 'García', 'mipass', 'mary123');
-
-    INSERT INTO `producto` (`nombre`,`precio`,`marca`,`descripcion`,`unidadesDisponibles`, `imagen`, `calificacion`, `comentario`,`correo`)
-    VALUES ('Celular', 20.00, 'Apple', 'Bonito celular 100% calidad', 11, '/home/vianey/Descargas/sample1.jpg', 4.2, 'Estuvo bien chafa, muy pirata', 'micorreito@feliz.com');
+    SELECT * FROM comentario;
+    SELECT * FROM calificacion;
+    
+    INSERT INTO `producto` (`nombre`,`precio`,`marca`,`descripcion`,`unidadesDisponibles`, `correo`)
+    VALUES ('Celular', 11000.00, 'Iphone', 'Bonito celular nuevo 100% calidad (no es clon)', 10, 'misalvaje14@gmail.com');
+    INSERT INTO `producto` (`nombre`,`precio`,`marca`,`descripcion`,`unidadesDisponibles`, `correo`)
+    VALUES ('Computadora', 10000.00, 'HP', 'Computadora HP nueva con SSD de 250 GB', 2, 'micorreito@gmail.com');
+    INSERT INTO `producto` (`nombre`,`precio`,`marca`,`descripcion`,`unidadesDisponibles`, `correo`)
+    VALUES ('PlayStation5', 20000.00, 'Sony', 'Consola de playstation 5 nueva ultimo modelo', 5, 'misalvaje14@gmail.com');
+	INSERT INTO `producto` (`nombre`,`precio`,`marca`,`descripcion`,`unidadesDisponibles`, `correo`)
+    VALUES ('Reloj', 500.00, 'Timex', 'Bonito reloj timex, muy preciso en los segundos', 20, 'elbuendicter@gmail.com');
+	
+     
+    INSERT INTO vendedor (`correo`, `nombre`, `apellidos`, `contrasena`,`nombreUsuario`)
+    VALUES ('misalvaje14@gmail.com', 'Jesus', 'Rodriguez Figueroa', 'JeRoFi', 'Jesus14');
+    INSERT INTO vendedor (`correo`, `nombre`, `apellidos`, `contrasena`,`nombreUsuario`)
+    VALUES ('micorreito@gmail.com', 'Jose', 'Garcia Santamaria', 'Pepegrillo', 'Pepegrillo1999');
+    INSERT INTO vendedor (`correo`, `nombre`, `apellidos`, `contrasena`,`nombreUsuario`)
+    VALUES ('elbuendicter@gmail.com', 'Dicter', 'Tadeo', 'mimejoramigoestoño', 'TadeoDicter');
+    
+    INSERT INTO comprador (`correo`, `nombre`, `apellidos`, `contrasena`,`nombreUsuario`)
+    VALUES ('toño@gmail.com', 'Antonio', 'Sandoval', 'Antony', 'ToñocomprasLocas');
+    INSERT INTO comprador (`correo`, `nombre`, `apellidos`, `contrasena`,`nombreUsuario`)
+    VALUES ('borras@gmail.com', 'Aylen', 'Pablito', 'vianey', 'PablitoBorras');
+	INSERT INTO comprador (`correo`, `nombre`, `apellidos`, `contrasena`,`nombreUsuario`)
+    VALUES ('santana@gmail.com', 'Jose', 'Garcia Santamaria', 'Pepe1234', 'SantanaGS');
+    
+    
+    INSERT INTO imagen (`id`, `imagen`) 
+    VALUES (1, "../../assets/img/play1.jpg");
+    INSERT INTO imagen (`id`, `imagen`) 
+    VALUES (1, "../../assets/img/play2.jpg");
+    INSERT INTO imagen (`id`, `imagen`) 
+    VALUES (2, "../../assets/img/reloj1.jpg");
+    INSERT INTO imagen (`id`, `imagen`) 
+    VALUES (3, "../../assets/img/Celular1.jpg");
+    INSERT INTO imagen (`id`, `imagen`) 
+    VALUES (4, "../../assets/img/computadora1.jpg");
+    
+    INSERT INTO comentario (`id`, `comentario`) 
+    VALUES (1, "Excelente Juego de playstation, muy buenos graficos");
+    INSERT INTO comentario (`id`, `comentario`) 
+    VALUES (2, "Bonito reloj muy indispensable");
+    INSERT INTO comentario (`id`, `comentario`) 
+    VALUES (3, "Bonito celular eficiente, no se traba");
+    INSERT INTO comentario (`id`, `comentario`) 
+    VALUES (3, "Excelente celular me salvo de apuros y es muy resistente");
+    INSERT INTO comentario (`id`, `comentario`) 
+    VALUES (4, "No se traba la computadora, 100% efectiva");
+    INSERT INTO comentario (`id`, `comentario`) 
+    VALUES (4, "Bonita Computadora y muy funcional" );
+    
+    INSERT INTO calificacion (`id`, `calificacion`) 
+    VALUES (1, 5);
+    INSERT INTO calificacion (`id`, `calificacion`) 
+    VALUES (1, 4.5);
+    INSERT INTO calificacion (`id`, `calificacion`) 
+    VALUES (2, 3);
+    INSERT INTO calificacion (`id`, `calificacion`) 
+    VALUES (2, 3.6);
+    INSERT INTO calificacion (`id`, `calificacion`) 
+    VALUES (3, 1.5);
+    INSERT INTO calificacion (`id`, `calificacion`) 
+    VALUES (4, 2.7);
+    
+    
+    SELECT * FROM vendedor WHERE (correo = "micorreito@gmail.com" AND contrasena = "Pepegrillo");
