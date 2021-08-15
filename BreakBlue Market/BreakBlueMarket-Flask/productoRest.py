2f12b683f4618698592028d066be52e5f01fbe65
tonodx17 <tonodx17@ciencias.unam.mx>
2021-08-14 22:00:48 -0500

    casosd de uso/pruebas sql postmman

--- BreakBlue Market/BreakBlueMarket-Flask/productoRest.py
+++ BreakBlue Market/BreakBlueMarket-Flask/productoRest.py
@@ -108,5 +108,16 @@ def agregar_opiyCalif(id):
         cursor.close()
         conn.close()
 
+@app.errorhandler(404)
+def not_found(error=None):
+    message = {
+        'status': 404,
+        'message': 'Not Found: ' + request.url,
+    }
+    resp = jsonify(message)
+    resp.status_code = 404
+
+    return resp
+
 if __name__ == "__main__":
     app.run()
